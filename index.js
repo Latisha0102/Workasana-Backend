57bdd5a42fdd8a21e580de190f2ea87462325992
Latisha (❁´◡`❁) <latishamehra5@gmail.com>
2025-11-15 22:40:30 +0530

    any changes made

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/auth/login", async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "4h" });
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "24h" });
     res
       .status(200)
       .json({ message: "Login Successfully", token, email: user.email });
